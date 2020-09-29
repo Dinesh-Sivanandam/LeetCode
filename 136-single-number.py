6dc7aa88fa461514eacea91bf23d4cef5423dfb7
Dinesh <61511078+Dinesh-Sivanandam@users.noreply.github.com>
2020-09-29 15:21:49 +0530

    Update 136-single-number.py

--- 136-single-number.py
+++ 136-single-number.py
@@ -1,7 +1,13 @@
+#Program to find the single number in the list that is not repeated
 def singleNumber(nums):
     """
     :type nums: List[int]
     :rtype: int
+    """
+    """
+    if the number not in new list adding the number to list
+    if the same number exist removing the number in the new list
+    then finally returning the value which is a single number in the list
     """
     no_duplicate_list = []
     for i in nums:
@@ -11,9 +17,12 @@ def singleNumber(nums):
             no_duplicate_list.remove(i)
     return no_duplicate_list.pop()
 
+#starting the main
 if __name__ == "__main__":
-
+    #declaring the value
     list1 = [2, 2, 1]
+    #calling the function and storing the result
     result = singleNumber(list1)
+    #printing the result
     print(result)
 
