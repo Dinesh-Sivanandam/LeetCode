d85067bcd0cf08acd21353e3d52298d93f9f91a4
Dinesh <61511078+Dinesh-Sivanandam@users.noreply.github.com>
2021-01-12 07:25:12 +0530

    345. Reverse Vowels of a String

---
+++ 345-reverse-vowels-of-a-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseVowels(self, s):
+        s = list(s)
+        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+        i, j = 0, len(s) - 1
+        
+        while i < j:
+            if s[i] not in vowels:
+                i += 1
+                continue
+                
+            if s[j] not in vowels:
+                j -= 1
+                continue
+                
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+            
+        return ''.join(s)
+    
+
+    
